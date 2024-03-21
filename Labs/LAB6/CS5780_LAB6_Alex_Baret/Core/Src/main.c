a1d8772bbebc7d2b98607da67192bbc066f4d635
Alex Baret <alexbaret7@icloud.com>
2024-03-20 19:42:14 -0600

    Initial attempt at P1

--- Labs/LAB6/CS5780_LAB6_Alex_Baret/Core/Src/main.c
+++ Labs/LAB6/CS5780_LAB6_Alex_Baret/Core/Src/main.c
@@ -1,115 +1,223 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+#define THRESHOLD1 0
+#define THRESHOLD2 10
+#define THRESHOLD3 20
+#define THRESHOLD4 30
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
+void setUp();
+void parseData(short data);
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+  setUp();
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+		parseData(ADC1->DR);
+		
   }
-  /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * Turns LEDs on/off depending on current x and y values.
+ */
+void parseData(short data)
+{
+
+  if (data <= THRESHOLD1)
+  {
+    GPIOC->ODR &= ~(1 << 6); // setting pin 6 RED to low
+    GPIOC->ODR &= ~(1 << 7); // setting pin 7 BLUE to low
+    GPIOC->ODR &= ~(1 << 9); // setting pin 9 GREEN to low
+    GPIOC->ODR &= ~(1 << 8); // setting pin 8 ORANGE to low
+  }
+
+  else if (data > THRESHOLD1 && data < THRESHOLD2) // red threshold
+  {
+    GPIOC->ODR |= (1 << 6); // setting pin 6 RED to high
+
+    GPIOC->ODR &= ~(1 << 7); // setting pin 6 BLUE to low
+    GPIOC->ODR &= ~(1 << 9); // setting pin 9 GREEN to low
+    GPIOC->ODR &= ~(1 << 8); // setting pin 8 ORANGE to low
+  }
+  else if (data > THRESHOLD2 && data < THRESHOLD3) // blue threshold
+  {
+    GPIOC->ODR |= (1 << 6); // setting pin 6 RED to high
+    GPIOC->ODR |= (1 << 7); // setting pin 7 BLUE to high
+
+    GPIOC->ODR &= ~(1 << 8); // setting pin 7 ORANGE to low
+    GPIOC->ODR &= ~(1 << 9); // setting pin 9 GREEN to low
+  }
+
+  else if (data > THRESHOLD3 && data < THRESHOLD4) // green threshold
+  {
+
+    GPIOC->ODR |= (1 << 6); // setting pin 6 RED to high
+    GPIOC->ODR |= (1 << 7); // setting pin 7 BLUE to high
+    GPIOC->ODR |= (1 << 8); // setting pin 8 ORANGE to high
+
+    GPIOC->ODR &= ~(1 << 9); // setting pin 9 GREEN to low
+  }
+  else if (data > THRESHOLD4) // orange threshold
+  {
+    GPIOC->ODR |= (1 << 6); // setting pin 6 RED to high
+    GPIOC->ODR |= (1 << 7); // setting pin 7 BLUE to high
+    GPIOC->ODR |= (1 << 8); // setting pin 8 ORANGE to high
+    GPIOC->ODR |= (1 << 9); // setting pin 9 GREEN to high
+  }
+}
+
+
+/**
+ * @brief Performs setup needed to run the program such as initializing peripheral clocks, setting GPIO modes, etc.
+ */
+void setUp()
+{
+  HAL_Init(); // Reset of all peripherals, Initializes the Flash interface and the Systick.
+
+  /* === Clock Settings === */
+  SystemClock_Config(); // Configure the system clock
+
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN; // Enable the system clock for the GPIOC peripheral
+ 
+
+  // /* === GPIO Settings === */
+
+  //GPIOC->OTYPER &= ~(1 << 0); // setting PC0 to push/pull output (clearing 0th bit for PC0 in OTYPER)
+
+
+  // configure the LEDs Pins
+  GPIOC->MODER |= (1 << 12); // setting PC6 to general output RED
+  GPIOC->MODER |= (1 << 14); // setting PC7 to general output BLUE
+  GPIOC->MODER |= (1 << 16); // setting PC8 to general output ORANGE
+  GPIOC->MODER |= (1 << 18); // setting PC9 to general output GREEN
+
+  GPIOC->OTYPER &= ~(0 << 7);  // setting PC6 to push/pull output
+  GPIOC->OTYPER &= ~(0 << 8);  // setting PC7 to push/pull output
+  GPIOC->OTYPER &= ~(0 << 9);  // setting PC8 to push/pull output
+  GPIOC->OTYPER &= ~(0 << 10); // setting PC9 to push/pull output
+
+  GPIOC->OSPEEDR &= ~(0 << 12); // setting PC6 to low speed
+  GPIOC->OSPEEDR &= ~(0 << 14); // setting PC7 to low speed
+  GPIOC->OSPEEDR &= ~(0 << 16); // setting PC8 to low speed
+  GPIOC->OSPEEDR &= ~(0 << 18); // setting PC9 to low speed
+
+  GPIOC->PUPDR &= ~(0 << 12); // setting PC6 to to no pull-up/down resistors
+  GPIOC->PUPDR &= ~(0 << 14); // setting PC7 to to no pull-up/down resistors
+  GPIOC->PUPDR &= ~(0 << 16); // setting PC8 to to no pull-up/down resistors
+  GPIOC->PUPDR &= ~(0 << 18); // setting PC9 to to no pull-up/down resistors
+
+  // Setting Pins initial states
+  GPIOC->ODR &= ~(1 << 6); // setting pin 6 to low
+  GPIOC->ODR &= ~(1 << 7); // setting pin 7 to low
+  GPIOC->ODR &= ~(1 << 8); // setting pin 8 to low
+  GPIOC->ODR &= ~(1 << 9); // setting pin 9 to low
+
+  /* === ADC Settings === */
+  // setting PC0 to analog
+  GPIOC->MODER |= (1 << 0); // setting 0th
+  GPIOC->MODER |= (1 << 1); // setting 1st
+
+  // setting PC0 to to no pull-up/down resistors
+  GPIOC->PUPDR &= ~(1 << 0); // clearing 0th bit
+  GPIOC->PUPDR &= ~(1 << 1); // clearing 1st bit
+	
+  RCC->APB2ENR |= RCC_APB2ENR_ADCEN; // Enable system clock for ADCEN peripheral
+  
+  // 8-bit
+  ADC1->CFGR1 |= (1 << 4);
+  ADC1->CFGR1 &= ~(1 << 3);
+	
+  // continuous conversion
+  ADC1->CFGR1 |= (1 << 13);
+	
+  // hardware triggers disabled
+  ADC1->CFGR1 &= ~(1 << 10);
+	ADC1->CFGR1 &= ~(1 << 11);
+	
+  // configuring the channel 0
+  ADC1->CR |= ADC_CHSELR_CHSEL10;
+
+  /* === ADC Calibration === */
+  /* (1) Ensure that ADEN = 0 */
+  /* (2) Clear ADEN by setting ADDIS*/
+  /* (3) Clear DMAEN */
+  /* (4) Launch the calibration by setting ADCAL */
+  /* (5) Wait until ADCAL=0 */
+
+  if ((ADC1->CR & ADC_CR_ADEN) != 0) /* (1) */
+  {
+    ADC1->CR |= ADC_CR_ADDIS; /* (2) */
+  }
+  while ((ADC1->CR & ADC_CR_ADEN) != 0)
+  {
+    /* For robust implementation, add here time-out management */
+  }
+  ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN;       /* (3) */
+  ADC1->CR |= ADC_CR_ADCAL;              /* (4) */
+  while ((ADC1->CR & ADC_CR_ADCAL) != 0) /* (5) */
+  {
+    /* For robust implementation, add here time-out management */
+  }
+	
+
+  /* (1) Ensure that ADRDY = 0 */
+  /* (2) Clear ADRDY */
+  /* (3) Enable the ADC */
+  /* (4) Wait until ADC ready */
+  if ((ADC1->ISR & ADC_ISR_ADRDY) != 0) /* (1) */
+  {
+    ADC1->ISR |= ADC_ISR_ADRDY; /* (2) */
+  }
+  ADC1->CR |= ADC_CR_ADEN;                 /* (3) */
+  while ((ADC1->ISR & ADC_ISR_ADRDY) == 0) /* (4) */
+  {
+    /* For robust implementation, add here time-out management */
+  }
+	
+	//start
+	ADC1->CR |= ADC_CR_ADSTART;
+	
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -120,9 +228,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -133,14 +240,10 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -152,14 +255,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
